db9cdfa1e2a86808f42c797556eab338717e5e3a
MSebb23 <matteosabbatini23@gmail.com>
2021-03-16 18:04:02 +0100

    Completed
    Da aggiungere : CSS, validazione numero già inserito (?)

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
+// ? Programma che chiede all'utente quanti numeri memorizzare e in quanto tempo
+ // ? Dopodichè aspetta tot secondi e ti chiede per n volte (n = elementi dell'array da indovinare) il numero.
+ // ? Dopo n volte ti dice quale è giusto e quale è sbagliato
+
 // variabili
 var array = [];
-var num = prompt("Memorizza i seguenti numeri, tra 30sec dovrai riscriverli" + array);
-
+var user_num = 0;
+var esito = "";
+var chances = prompt("quanti numeri vuoi memorizzare?");
+chances = isNumber(chances);
+var win = 0;
+var value = parseInt(prompt("Quanto tempo impiegheresti per memorizzare questi " + chances + " numeri"));
+alert ("I numeri da ricordare sono : " + array)
+value = isNumber(value) * 1000;
+console.log(value);
 
 // corpo del programma
-num = isNumber(num);
 // time = isNumber(time);
-    //* 1. genera ed esponi 5 numeri casuali
-array = getRandom(0, 100, 5);
-console.log(array)
-
-
-
-
-
-
-
-
-
+    // ! 1. genera ed esponi 5 numeri casuali
+array = getRandom(0, 100, chances);
+console.log(array);
+    // !  2. timer 30 sec
+    timer(value); //funzione che ti chiede di indovinare choice numeri dopo value millisecondi
 
 // definizioni funzioni
 function timer(value){
     setTimeout(() => {
-        console.log("sono passati 3ss")
-    }, 3000);
+        // !  3. inserisci uno alla volta i numeri
+        for(var y = 0; y < chances; y++){
+            console.log(chances + " win " + win)
+            user_num = parseInt(prompt("Indovina i numeri \n tentativi rimanenti : " + (chances - win)));
+            user_num = isNumber(user_num);
+            // !  4. controlla quanti ne hai fatti giusti e quanti sbagliati
+            if (array.includes(user_num)){
+                esito += "<br>" + user_num + " è nella lista";
+                win += 1;
+            } else {
+                esito += "<br>" + user_num + " NON è nella lista";
+            }
+        }
+        
+                esito += "<br>" + "hai indovinato " + win + " su " + chances;
+        
+            document.getElementById("outcome").innerHTML = esito
+        console.log("sono passati " + value + "secondi")
+    }, value);
 }
 
 function isNumber(num) {
     var num = parseInt(num);
-    while (isNaN(num) != true && num <= 0) {
-        num = parseInt(prompt("Hello"));
+    while (!isNaN(num) == false || num <= 0) {
+       num = parseInt(prompt("Hai inserito un valore errato, inserisci un intero positivo"));
     }
         return num;
-
 }
 
 function getRandom(min, max, num) {
@@ -43,6 +62,3 @@ function getRandom(min, max, num) {
     return value_array;
 }
 
-// 2. timer 30 sec
-// 3. inserisci uno alla volta i 5 numeri
-// 4. controlla quanti ne hai fatti giusti e quanti sbagliati
